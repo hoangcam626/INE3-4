9d58bddc836ee4f586348920b9ce07272f8e03e4
Nhunghh <nhunghh@hivetech.vn>
2023-06-22 13:25:58 +0700

    logger

--- src/main/java/com/hivetech/Ine3Application.java
+++ src/main/java/com/hivetech/Ine3Application.java
@@ -1,12 +1,19 @@
 package com.hivetech;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Ine3Application {
-
+    private static final Logger logger = LoggerFactory.getLogger(Ine3Application.class);
     public static void main(String[] args) {
+        logger.trace("A TRACE Message");
+        logger.debug("A DEBUG Message");
+        logger.info("An INFO Message");
+        logger.warn("A WARN Message");
+        logger.error("An ERROR Message");
         SpringApplication.run(Ine3Application.class, args);
     }
 
